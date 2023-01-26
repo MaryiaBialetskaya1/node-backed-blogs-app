aeba3dbe4a851ce1065712a92f88326af02d02dc
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2023-01-26 18:57:35 -0500

    update index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,37 @@
 import express, {Request, Response} from 'express'
-import {productsRouter} from "./routers/products-router";
-const app = express()
-const port = process.env.PORT || 3003
+import bodyParser from 'body-parser'
+import {blogsRouter} from "./routers/blogs-router";
+import {postsRouter} from "./routers/posts-router";
+import {blogCollection, postCollection, runDb} from "./repositories/db";
 
-const parserMiddleware = express.json()
-app.use(parserMiddleware)
+const app = express()
+const jsonBodyMiddleware = bodyParser.json()
+app.use(jsonBodyMiddleware)
+
+const port = process.env.PORT || 3000
+
+app.use('/blogs', blogsRouter)
+app.use('/posts', postsRouter)
 
 app.get('/', (req:Request, res:Response) => {
-    res.send('Hello blogs project!!!')
+    res.send('Hello from IT-Incubator!!!!')
 })
 
-app.use('/products', productsRouter)
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.delete( '/testing/all-data', async (req: Request, res:Response) =>{
+    await blogCollection.deleteMany({});
+    await postCollection.deleteMany({});
+    res.send(204)
 })
+
+
+const startApp = async () =>{
+    await runDb()
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
+}
+startApp()
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// })
